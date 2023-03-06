d99b91daf1243db524586ad38dddbfbc609f0375
unknown <gustavohenriquebs7@gmail.com>
2023-03-06 19:38:03 -0300

    variaveis

---
+++ gustavo.py
@@ -0,0 +1,16 @@
+nome = "Gustavo"
+sobrenome = "Bednartchuk"
+idade = 18 
+altura = 1.80
+peso = 65
+
+
+maior_idade = idade >= 18
+
+
+print("Nome:", nome)
+print("Sobrenome:", sobrenome)
+print("Idade:", idade)
+print("Altura:", altura)
+print("Peso:", peso)
+print("Maior de idade:", maior_idade)
